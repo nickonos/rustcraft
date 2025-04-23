804ad40e01d5f6a98901ba0cbc5bccb667bc732e
nickonos <nick.van.oijen@gmail.com>
2025-04-23 14:10:17 +0200

    Added creation of swap chain

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,15 @@
     clippy::unnecessary_wraps
 )]
 
+use std::sync::Arc;
+
 use anyhow::Result;
-use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo, QueueFlags};
+use vulkano::device::physical::PhysicalDeviceType;
+use vulkano::device::{Device, DeviceCreateInfo, DeviceExtensions, QueueCreateInfo, QueueFlags};
+use vulkano::image::ImageUsage;
 use vulkano::instance::{Instance, InstanceCreateFlags, InstanceCreateInfo};
-use vulkano::library;
+use vulkano::{library, swapchain};
+use vulkano::swapchain::{Surface, Swapchain, SwapchainCreateInfo};
 use winit::application::ApplicationHandler;
 use winit::dpi::LogicalSize;
 use winit::event::WindowEvent;
@@ -45,34 +50,67 @@ impl ApplicationHandler for App {
 
 fn main() -> Result<()> {
     let event_loop = EventLoop::new().expect("Failed to create event loop");
-    let mut app = App::default();
-
-    event_loop.run_app(&mut app)?;
 
     // Create VKInstance
     let library = library::VulkanLibrary::new().expect("No local Vulkan library found");
+    let required_extensions =
+        Surface::required_extensions(&event_loop).expect("unable to get required extensions");
 
     let instance_create_info = InstanceCreateInfo {
         flags: InstanceCreateFlags::ENUMERATE_PORTABILITY,
+        enabled_extensions: required_extensions,
         ..Default::default()
     };
 
     let instance = Instance::new(library, instance_create_info).expect("Failed to create instance");
 
+    // Make sure physical device has swapchain support
+    let device_extensions = DeviceExtensions {
+        khr_swapchain: true,
+        ..DeviceExtensions::empty()
+    };
+
+    // Create window
+    let mut app = App::default();
+    event_loop.run_app(&mut app)?;
+
+    let window = app.window.expect("window was not created");
+
+    let dimenisons = window.inner_size();
+
+    // Create Window Surface
+    let surface = Surface::from_window(instance.clone(), Arc::new(window))
+        .expect("Unable to create surface from window");
+
     // Select Physical Device
-    let physical_device = instance
+    let (physical_device, queue_family_index) = instance
         .enumerate_physical_devices()
         .expect("Could not enumerate physical devices")
-        .next()
+        .filter(|p| p.supported_extensions().contains(&device_extensions))
+        .filter_map(|p| {
+            p.queue_family_properties()
+                .iter()
+                .enumerate()
+                .position(|(i, q)| {
+                    q.queue_flags.contains(QueueFlags::GRAPHICS)
+                        && p.surface_support(i as u32, &surface).unwrap_or(false)
+                })
+                .map(|q| (p, q as u32))
+        })
+        .min_by_key(|(p, _)| match p.properties().device_type {
+            PhysicalDeviceType::DiscreteGpu => 0,
+            PhysicalDeviceType::IntegratedGpu => 1,
+            PhysicalDeviceType::VirtualGpu => 2,
+            PhysicalDeviceType::Cpu => 3,
+            _ => 4,
+        })
         .expect("Could not find a physical device");
 
-    // Get Queue Family Index
-    let queue_family_index = physical_device
-        .queue_family_properties()
-        .iter()
-        .position(|properties| properties.queue_flags.contains(QueueFlags::GRAPHICS))
-        .expect("Could not find a queue family that supports graphics")
-        as u32;
+    
+
+    let capabilities = physical_device.surface_capabilities(&surface, Default::default()).expect("could not get physical device capabilities");
+    let composite_alpha = capabilities.supported_composite_alpha.into_iter().next().unwrap();
+    let image_format = physical_device.surface_formats(&surface, Default::default()).unwrap()[0].0;
 
     // Create Logical Device
     let (device, mut queues) = Device::new(
@@ -82,6 +120,7 @@ fn main() -> Result<()> {
                 queue_family_index,
                 ..Default::default()
             }],
+            enabled_extensions: device_extensions,
             ..Default::default()
         },
     )
@@ -89,10 +128,16 @@ fn main() -> Result<()> {
 
     let queue = queues.next().unwrap();
 
-    // Create Window Surface
-
     // Create Swapchain
-
+    let (mut swapchain, images) = Swapchain::new(device.clone(), surface.clone(), SwapchainCreateInfo {
+        min_image_count: capabilities.min_image_count + 1,
+        image_format: image_format,
+        image_extent: dimenisons.into(),
+        image_usage: ImageUsage::COLOR_ATTACHMENT,
+        composite_alpha: composite_alpha,
+        ..Default::default()
+    }).expect("Unable to create swapchain");
+    
     // Create Image Views
 
     // Create Framebuffers
