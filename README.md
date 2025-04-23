2baf3f560d68ddf3398c318b733b07b09e0c067a
nickonos <nick.van.oijen@gmail.com>
2025-04-23 13:03:49 +0200

    Get physical device and logical device

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rustcraft
+
+This 
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@
 )]
 
 use anyhow::Result;
+use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo, QueueFlags};
+use vulkano::instance::{Instance, InstanceCreateFlags, InstanceCreateInfo};
+use vulkano::library;
 use winit::application::ApplicationHandler;
 use winit::dpi::LogicalSize;
 use winit::event::WindowEvent;
@@ -21,10 +24,8 @@ impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
         let window_attributes = Window::default_attributes()
             .with_title("Minecraft, Also try Terraria!")
-            .with_inner_size(LogicalSize::new(800, 600))
-            ;
+            .with_inner_size(LogicalSize::new(800, 600));
 
-        
         self.window = Some(ActiveEventLoop::create_window(event_loop, window_attributes).unwrap());
     }
 
@@ -33,7 +34,7 @@ impl ApplicationHandler for App {
             WindowEvent::CloseRequested => {
                 println!("The close button was pressed {:?}", id);
                 event_loop.exit();
-            },
+            }
             WindowEvent::RedrawRequested => {
                 self.window.as_ref().unwrap().request_redraw();
             }
@@ -43,12 +44,68 @@ impl ApplicationHandler for App {
 }
 
 fn main() -> Result<()> {
-
-    let event_loop = EventLoop::new()?;
+    let event_loop = EventLoop::new().expect("Failed to create event loop");
     let mut app = App::default();
 
     event_loop.run_app(&mut app)?;
 
+    // Create VKInstance
+    let library = library::VulkanLibrary::new().expect("No local Vulkan library found");
+
+    let instance_create_info = InstanceCreateInfo {
+        flags: InstanceCreateFlags::ENUMERATE_PORTABILITY,
+        ..Default::default()
+    };
+
+    let instance = Instance::new(library, instance_create_info).expect("Failed to create instance");
+
+    // Select Physical Device
+    let physical_device = instance
+        .enumerate_physical_devices()
+        .expect("Could not enumerate physical devices")
+        .next()
+        .expect("Could not find a physical device");
+
+    // Get Queue Family Index
+    let queue_family_index = physical_device
+        .queue_family_properties()
+        .iter()
+        .position(|properties| properties.queue_flags.contains(QueueFlags::GRAPHICS))
+        .expect("Could not find a queue family that supports graphics")
+        as u32;
+
+    // Create Logical Device
+    let (device, mut queues) = Device::new(
+        physical_device,
+        DeviceCreateInfo {
+            queue_create_infos: vec![QueueCreateInfo {
+                queue_family_index,
+                ..Default::default()
+            }],
+            ..Default::default()
+        },
+    )
+    .expect("Failed to create logical device");
+
+    let queue = queues.next().unwrap();
+
+    // Create Window Surface
+
+    // Create Swapchain
+
+    // Create Image Views
+
+    // Create Framebuffers
+
+    // Create Render Pass
+
+    // Create Graphics Pipeline
+
+    // Create Command Pool
+
+    // Create Command Buffers
+
+    // Create Main loop
+
     Ok(())
 }
-
