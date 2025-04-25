06bdf6b1451c6cc07430319b89af9d159483aaf8
nickonos <nick.van.oijen@gmail.com>
2025-04-25 18:12:48 +0200

    Added World to game

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,22 @@
     clippy::unnecessary_wraps
 )]
 
-
 mod engine;
+mod game;
 mod model;
+
+use std::sync::Arc;
 
 use anyhow::Result;
 use engine::setup::ApplicationEngine;
+use game::world::World;
 use winit::event_loop::EventLoop;
 
 fn main() -> Result<()> {
+    let world = Arc::new(World::new());
+
     let event_loop = EventLoop::new().expect("Failed to create event loop");
-    let mut app = ApplicationEngine::new(&event_loop);
+    let mut app = ApplicationEngine::new(&event_loop, world);
     event_loop.run_app(&mut app)?;
 
     Ok(())
